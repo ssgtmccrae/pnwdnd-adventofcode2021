bb06c503e423352896a3cacf72a43a2981f65b5d
Michael Wanek <mwanek@uw.edu>
2021-12-01 12:58:04 -0800

    Day 1 solutions

---
+++ mwanek/day1/day1.py
@@ -0,0 +1,32 @@
+import pathlib
+path_to_input = pathlib.Path('day1_input.txt')
+
+with path_to_input.open('r') as file:
+    day1_raw_input = file.read()
+
+day1_input = [int(i) for i in day1_raw_input.split()]
+
+def get_increases(list_to_count):
+    increases = 0
+    for index, value in enumerate(list_to_count):
+        if index > 0:
+            prev = index - 1
+            if value > list_to_count[prev]:
+                increases += 1
+    return increases
+
+def make_triplet_sums(list_to_sum):
+    sums = []
+    for index, value in enumerate(list_to_sum):
+        if index + 3 <= len(list_to_sum):
+            one = list_to_sum[index + 1]
+            two = list_to_sum[index + 2]
+            sums.append(value + one + two)
+    return sums
+
+
+part1_answer = get_increases(day1_input)
+part2_answer = get_increases(make_triplet_sums(day1_input))
+
+print("Part 1: Depth increases =", part1_answer)
+print("Part 2: Measurement increases =", part2_answer)
