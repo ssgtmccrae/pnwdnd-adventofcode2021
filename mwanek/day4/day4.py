421be0afa8bcff814b16c8293d9fb054ab87cd04
Michael Wanek <mwanek@uw.edu>
2021-12-04 16:37:11 -0800

    "Happy" with day4 now.

--- mwanek/day4/day4.py
+++ mwanek/day4/day4.py
@@ -1,119 +1,118 @@
-from aocd import data as day4_input # pylint: disable=no-name-in-module
-import math
-import itertools as it
-import re
+from aocd import data as bingo_data
+from typing import List
 from dataclasses import dataclass
 from termcolor import colored
-
-test_input = """7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
-
-22 13 17 11  0
- 8  2 23  4 24
-21  9 14 16  7
- 6 10  3 18  5
- 1 12 20 15 19
-
- 3 15  0  2 22
- 9 18 13 17  5
-19  8  7 25 23
-20 11 10 24  4
-14 21 16 12  6
-
-14 21 17 24  4
-10 16 15  9 19
-18  8 23 26 20
-22 11 13  6  5
- 2  0 12  3  7
-"""
-
-
+import re
 @dataclass
 class BingoCoord:
-    value: int
+    x: int
+    y: int
+    number: int
     marked = False
-    def __eq__(self, other):
-        return self.value == other
 
 class BingoBoard:
-    def __init__(self, coords) -> None:
-        # take the 5 lines and put them into coords
-        # convert them to 5 lists of 5
-        # list LOOP_NUMBER, INDEX ()
-        # (0, 0) : BingoCoord(22) -> (4, 0) : BingoCoord(00)
-        # (0, 4) : BingoCoord(01) -> (4, 4) : BingoCoord(19)
-        lines = [l for l in coords.split("\n") if l]
-        self.board = {}
-        for row, values in enumerate(lines):
-            for column, value in enumerate(values.split()):
-                self.board[row, column] = BingoCoord(int(value))
-        self.total_marked = 0 # Don't check for bingos unless 5
+    def __init__(self, board_data: list) -> None:
+        self.coords = []
+        for y, coord_numbers in enumerate(board_data):
+            for x, number in enumerate(coord_numbers):
+                self.coords.append(BingoCoord(x, y, number))
+        self.total_marks = 0
+        self.board_size = 5
+        self.bingo = False
+        self.final_mark = False
 
-    def mark_value(self, number_to_mark):
-        my_coord = [ coord for coord, value in self.board.items() if number_to_mark == value ]
-        if my_coord:
-            my_coord = my_coord[0]
-            self.total_marked += 1
-            self.board[my_coord].marked = True
-            #print(f"I marked {number_to_mark} on this board: ")
-            #self.print_board()
-            #input()
-            return True
-        return False
+    def get_number(self, number: int) -> BingoCoord:
+        matching_coord = [coord for coord in self.coords if coord.number == number]
+        return matching_coord[0] if len(matching_coord) == 1 else False
 
-    def find_bingo(self):
-        if self.total_marked >= 5:
-            for i in range(5):
-                if all([value.marked for coord, value in self.board.items() if coord[0] == i]):
-                    #print(f"Horizontal Bingo! Sum: {str(self.board_sum)}")
-                    return True
-                elif all([value.marked for coord, value in self.board.items() if coord[1] == i]):
-                    #print(f"Vertical Bingo! Sum: {str(self.board_sum)}")
-                    return True
-        return False
+    def get_row(self, y: int) -> List[BingoCoord]:
+        coords = [coord for coord in self.coords if coord.y == y]
+        return coords if len(coords) == self.board_size else False
 
+    def get_column(self, x: int) -> List[BingoCoord]:
+        coords = [coord for coord in self.coords if coord.x == x]
+        return coords if len(coords) == self.board_size else False
 
-    def print_board(self):
-        print()
-        for i in range(5):
-            print()
-            for j in range(5):
-                if self.board[i, j].marked:
-                    print(colored("{:>2}".format(str(self.board[i, j].value)), on_color="on_green", attrs=['bold']), end=" ")
+    def get_coord(self, x: int, y: int) -> BingoCoord:
+        matching_coord = [coord for coord in self.coords if coord.x == x and coord.y == y]
+        return matching_coord[0] if len(matching_coord) == 1 else False
+
+    def state(self) -> None:
+        for y in range(self.board_size):
+            print(end=" ")
+            for coord in self.get_row(y):
+                if coord.marked:
+                    print(colored(f"{coord.number:>2}", on_color="on_green", attrs=['bold']), end=" ")
                 else:
-                    print("{:2}".format(str(self.board[i,j].value)), end=" ")
-        print()
+                    print(f"{coord.number:>2}", end=" ")
+            print()
 
     @property
-    def board_sum(self):
-        return sum([value.value for value in self.board.values() if not value.marked])
+    def unmarked_sum(self) -> int:
+        return sum([coord.number for coord in self.coords if not coord.marked])
 
-split_input = day4_input.split('\n\n')
-numbers_to_call = split_input.pop(0).strip().split(',')
-boards = split_input
+    def mark(self, number: int) -> bool:
+        if not self.bingo:
+            coord = self.get_number(number)
+            if coord:
+                coord.marked = True
+                self.total_marks += 1
+                self.check_for_bingo(coord)
+                return True
+        return False
 
-#board_regex = re.compile(r"((?:(?:[ \d]{1,2} {0,2}){5}\n){5})")
-#boards = re.findall(board_regex, day4_input)
+    def check_for_bingo(self, last_mark: BingoCoord) -> None:
+        if self.total_marks >= self.board_size:
+            if all([coord.marked for coord in self.get_row(last_mark.y)]):
+                self.final_mark = last_mark.number
+                self.bingo = True
+            if all([coord.marked for coord in self.get_column(last_mark.x)]):
+                self.final_mark = last_mark.number
+                self.bingo = True
 
-#numbers_to_call_regex = re.compile(r"(?:\d{1,2},)+\d{1,2}\n")
-#numbers_to_call = re.match(numbers_to_call_regex, day4_input).group(0).strip().split(',')
+class BingoHall:
+    def __init__(self, bingo_boards: list, numbers_to_call: list) -> None:
+        # Create boards from data
+        # Create numbers to call from data
+        self.boards = [BingoBoard(board) for board in bingo_boards]
+        self.finished_boards = []
+        self.numbers_to_call = numbers_to_call
 
-bingo_boards = [BingoBoard(board) for board in boards]
+    def announce_number(self, number: int) -> None:
+        for board in self.boards:
+            board.mark(number)
+            if board.bingo and board not in self.finished_boards:
+                self.finished_boards.append(board)
 
-#for board in bingo_boards:
-#    board.mark_value(16)
-#    print([value.marked for coord, value in board.board.items() if value == 16])
-
-for num in numbers_to_call:
-    if not bingo_boards:
-        break
-    #print(f"Calling: {num}")
-    for board in bingo_boards[:]:
-        marked = board.mark_value(int(num))
-        if marked:
-            if board.find_bingo():
-                bingo_boards.remove(board)
-                #print("Board sum:", int(num) * board.board_sum)
-            if not bingo_boards:
-                print("Last sum:", int(num) * board.board_sum)
+    def run_bingo(self) -> None:
+        for number in self.numbers_to_call:
+            if len(self.boards) is not len(self.finished_boards):
+                self.announce_number(number)
+            else:
                 break
-        marked = False
+
+    @property
+    def winner(self) -> BingoBoard:
+        return self.finished_boards[0]
+
+    @property
+    def last_winner(self) -> BingoBoard:
+        return self.finished_boards[-1]
+
+bingo_lines = bingo_data.split('\n')
+bingo_numbers = [int(x) for x in bingo_lines[0].split(',')]
+
+parse = re.compile("[-+]?\d+")
+to_ints = lambda s: list(map(int, parse.findall(s)))
+bingo_boards = []
+for i in range(2, len(bingo_lines), 6):
+    bingo_boards.append(list(map(to_ints, bingo_lines[i:i+5])))
+
+bingo_hall = BingoHall(bingo_boards, bingo_numbers)
+bingo_hall.run_bingo()
+print("First winner's board:")
+bingo_hall.winner.state()
+print("First winner's score:", bingo_hall.winner.unmarked_sum * bingo_hall.winner.final_mark, "\n")
+print("Final winner's board:")
+bingo_hall.last_winner.state()
+print("Final winner's score:", bingo_hall.last_winner.unmarked_sum * bingo_hall.last_winner.final_mark)
