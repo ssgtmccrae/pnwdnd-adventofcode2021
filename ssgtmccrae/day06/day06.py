eace033e005ad22479036339e60fa47117325c0d
Ryan McGregor <ryanmcgr@protonmail.com>
2021-12-06 23:36:52 -0800

    Linting

--- ssgtmccrae/day06/day06.py
+++ ssgtmccrae/day06/day06.py
@@ -5,36 +5,35 @@ https://adventofcode.com/2021/day/6
 """
 from pprint import pprint
 import sys
-import numpy as np
 
-def run_counter(generations: int, starting_set: list[int]):
+def run_counter(_generations: int, starting_set: list[int]):
     """
-    Runs a counter for 'generations' number of events, where each item in starting_set' spawns a child
+    Runs a _counter for '_generations' number of events, where each item in starting_set' spawns a child
     which will produce another child in 8 days.
-    Input: generations (int), starting_set (List(int))
-    Output: dict containing counters for each 'time_to_breed'
+    Input: _generations (int), starting_set (List(int))
+    Output: dict containing _counters for each 'time_to_breed'
     """
-    counter = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0}
+    _counter = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0}
 
     for number in starting_set:
-        counter[int(number)] += 1
+        _counter[int(number)] += 1
 
     print('Initial state:')
-    pprint(counter)
+    pprint(_counter)
 
     current_gen = 0
-    while current_gen < generations:
-        for key in counter.keys():
+    while current_gen < _generations:
+        for key in sorted(_counter):
             if key == 0:
-                counter[9] = counter[0]
-                counter[7] += counter[0]
+                _counter[9] = _counter[0]
+                _counter[7] += _counter[0]
             if key <= 8:
-                counter[key] = counter[key + 1]
-        counter[9] = 0
+                _counter[key] = _counter[key + 1]
+        _counter[9] = 0
         current_gen += 1
         print(f'gen: {current_gen}')
-        pprint(counter)
-    return counter
+        pprint(_counter)
+    return _counter
 
 
 if __name__ == '__main__':
