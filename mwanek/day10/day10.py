cf6033a44cc52160d3da52109ee2cf32ed4cc5ae
Michael Wanek <mwanek@uw.edu>
2021-12-10 15:08:23 -0800

    Moar readability!

--- mwanek/day10/day10.py
+++ mwanek/day10/day10.py
@@ -13,37 +13,39 @@ pairs = [
         Pair("<", ">")
         ]
 
-opening_chars = [pair.opener for pair in pairs]
-closing_chars = [pair.closer for pair in pairs]
-opener_for = dict(zip(closing_chars, opening_chars))
-closer_for = dict(zip(opening_chars, closing_chars))
-syntax_error_scores = dict(zip(closing_chars, [3,57,1197,25137]))
-comp_scores = dict(zip(opening_chars, [1,2,3,4]))
+openers = [pair.opener for pair in pairs]
+closers = [pair.closer for pair in pairs]
+opener_for = dict(zip(closers, openers))
+closer_for = dict(zip(openers, closers))
+syntax_error_score_for = dict(zip(closers, [3,57,1197,25137]))
+completion_score_for = dict(zip(openers, [1,2,3,4]))
 
 syntax_error_total = 0
-comp_score_totals = []
+completion_score_totals = []
 
 for line in data:
     my_stack = deque()
     syntax_error = False
-    for char in line:
-        if char in opening_chars:
-            my_stack.append(char)
-        elif char in closing_chars:
+    for character in line:
+        if character in openers:
+            my_stack.append(character)
+        elif character in closers:
             opener = my_stack.pop()
-            if opener != opener_for[char]:
-                syntax_error_total += syntax_error_scores[char]
+            if opener is not opener_for[character]:
+                syntax_error_total += syntax_error_score_for[character]
                 syntax_error = True
                 break
+        else:
+            raise KeyError
     if not syntax_error:
-        my_comp_score = 0
+        my_completion_score = 0
         while len(my_stack) > 0:
-            incomplete_char = my_stack.pop()
-            my_comp_score *= 5
-            my_comp_score += comp_scores[incomplete_char]
-        comp_score_totals.append(my_comp_score)
+            opener = my_stack.pop()
+            my_completion_score *= 5
+            my_completion_score += completion_score_for[opener]
+        completion_score_totals.append(my_completion_score)
 
-median_comp_score = int(np.median(comp_score_totals))
+median_completion_score = round(np.median(completion_score_totals))
 
 print(f"Part 1, syntax error score: {syntax_error_total}")
-print(f"Part 2, median autocomplete score: {median_comp_score}")
+print(f"Part 2, median autocomplete score: {median_completion_score}")
