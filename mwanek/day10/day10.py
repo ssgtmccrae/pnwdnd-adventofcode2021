c82ba738d45cd4943a3ab9bebb3d499f5540c4d3
Michael Wanek <mwanek@uw.edu>
2021-12-10 11:29:46 -0800

    With zips!

--- mwanek/day10/day10.py
+++ mwanek/day10/day10.py
@@ -3,51 +3,36 @@ import numpy as np
 from collections import deque
 
 with open("input.txt", "r") as file:
-    data = file.read()
+    data = file.read().strip().split('\n')
 
-data = data.strip().split('\n')
+Pair = namedtuple("Pair", "opener, closer")
+pairs = [
+        Pair("(", ")"),
+        Pair("[", "]"),
+        Pair("{", "}"),
+        Pair("<", ">")
+        ]
 
-Chars = namedtuple("Chars", "openers, closers, error_scores, comp_scores")
-CHARS = Chars(
-            openers=("(", "[", "{", "<"),
-            closers=(")", "]", "}", ">"),
-            error_scores=(3, 57, 1197, 25137),
-            comp_scores=(1,2,3,4)
-            )
+opening_chars = [pair.opener for pair in pairs]
+closing_chars = [pair.closer for pair in pairs]
+opener_for = dict(zip(closing_chars, opening_chars))
+closer_for = dict(zip(opening_chars, closing_chars))
+syntax_error_scores = dict(zip(closing_chars, [3,57,1197,25137]))
+comp_scores = dict(zip(opening_chars, [1,2,3,4]))
 
-def chunk_match(char: str) -> str:
-    if char in CHARS.openers:
-        return CHARS.closers[CHARS.openers.index(char)]
-    elif char in CHARS.closers:
-        return CHARS.openers[CHARS.closers.index(char)]
-    else:
-        raise ValueError
-
-def error_score(char: str) -> int:
-    if char in CHARS.closers:
-        return CHARS.error_scores[CHARS.closers.index(char)]
-    else:
-        raise ValueError
-
-def comp_score(char: str) -> int:
-    if char in CHARS.openers:
-        return CHARS.comp_scores[CHARS.openers.index(char)]
-    else:
-        raise ValueError
-
-syntax_error_score = 0
-comp_scores = []
+syntax_error_total = 0
+comp_score_totals = []
 
 for line in data:
     my_stack = deque()
     syntax_error = False
     for char in line:
-        if char in CHARS.openers:
+        if char in opening_chars:
             my_stack.append(char)
-        elif char in CHARS.closers:
-            match_char = my_stack.pop()
-            if match_char != chunk_match(char):
-                syntax_error_score += error_score(char)
+        elif char in closeing_chars:
+            opener = my_stack.pop()
+            if opener != opener_for[char]:
+                syntax_error_total += syntax_error_scores[char]
                 syntax_error = True
                 break
     if not syntax_error:
@@ -55,10 +40,10 @@ for line in data:
         while len(my_stack) > 0:
             incomplete_char = my_stack.pop()
             my_comp_score *= 5
-            my_comp_score += comp_score(incomplete_char)
-        comp_scores.append(my_comp_score)
+            my_comp_score += comp_scores[incomplete_char]
+        comp_score_totals.append(my_comp_score)
 
-median_comp_score = int(np.median(comp_scores))
+median_comp_score = int(np.median(comp_score_totals))
 
-print(f"Part 1, syntax error score: {syntax_error_score}")
+print(f"Part 1, syntax error score: {syntax_error_total}")
 print(f"Part 2, median autocomplete score: {median_comp_score}")
