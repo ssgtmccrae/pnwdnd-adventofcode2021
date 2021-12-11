3192424e9f0d818e349b9c54601164713353793e
Michael Wanek <mwanek@uw.edu>
2021-12-10 20:15:58 -0800

    Okay last commit.

--- mwanek/day10/day10.py
+++ mwanek/day10/day10.py
@@ -1,3 +1,6 @@
+"""
+Advent of Code, 2021: Puzzle 10
+"""
 from collections import namedtuple
 from collections import deque
 import numpy as np
@@ -6,46 +9,43 @@ with open("input.txt", "r", encoding="utf-8") as file:
     data = file.read().strip().split('\n')
 
 Pair = namedtuple("Pair", "opener, closer")
-pairs = [
+PAIRS = [
         Pair("(", ")"),
         Pair("[", "]"),
         Pair("{", "}"),
         Pair("<", ">")
         ]
+OPENERS = [pair.opener for pair in PAIRS]
+CLOSERS = [pair.closer for pair in PAIRS]
+OPENER_FOR = dict(zip(CLOSERS, OPENERS))
+SYNTAX_ERROR_SCORE_FOR = dict(zip(CLOSERS, [3,57,1197,25137]))
+COMPLETION_SCORE_FOR = dict(zip(OPENERS, [1,2,3,4]))
 
-openers = [pair.opener for pair in pairs]
-closers = [pair.closer for pair in pairs]
-opener_for = dict(zip(closers, openers))
-closer_for = dict(zip(openers, closers))
-syntax_error_score_for = dict(zip(closers, [3,57,1197,25137]))
-completion_score_for = dict(zip(openers, [1,2,3,4]))
-
-syntax_error_total = 0
-completion_scores = []
+syntax_error_score = 0      #pylint: disable=invalid-name
+completion_scores = []      #pylint: disable=invalid-name
 
 for line in data:
     my_stack = deque()
-    syntax_error = False
     try:
         for character in line:
-            if character in openers:
+            if character in OPENERS:
                 my_stack.append(character)
-            elif character in closers:
+            elif character in CLOSERS:
                 opener = my_stack.pop()
-                if opener is not opener_for[character]:
+                if opener is not OPENER_FOR[character]:
                     raise SyntaxError
             else:
                 raise KeyError
-        my_completion_score = 0
+        my_completion_score = 0     #pylint: disable=invalid-name
         while len(my_stack) > 0:
             opener = my_stack.pop()
             my_completion_score *= 5
-            my_completion_score += completion_score_for[opener]
+            my_completion_score += COMPLETION_SCORE_FOR[opener]
         completion_scores.append(my_completion_score)
     except SyntaxError:
-        syntax_error_total += syntax_error_score_for[character]
+        syntax_error_score += SYNTAX_ERROR_SCORE_FOR[character]
 
 median_completion_score = round(np.median(completion_scores))
 
-print(f"Part 1, syntax error score: {syntax_error_total}")
+print(f"Part 1, syntax error score: {syntax_error_score}")
 print(f"Part 2, median autocomplete score: {median_completion_score}")
