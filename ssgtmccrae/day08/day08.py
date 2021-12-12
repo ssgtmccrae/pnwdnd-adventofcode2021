42825bb95b2390594e0efcb9d38061895c20b82f
Ryan McGregor <ryanmcgr@uw.edu>
2021-12-12 12:46:13 -0800

    Pt2 Working

--- ssgtmccrae/day08/day08.py
+++ ssgtmccrae/day08/day08.py
@@ -6,22 +6,9 @@ Been really busy, tornadoes and stuff...
 """
 from typing import List, OrderedDict
 from aocd import get_data
-import sys
-from pprint import pprint
 
-test_set =  [
-              {'debug_code': ['acef',
-                           'egfcbd',
-                           'fe',
-                           'dgbfa',
-                           'caegdb',
-                           'gcdefba',
-                           'faged',
-                           'aegcd',
-                           'gfe',
-                           'gfaecd'],
-            'output': ['dafcge', 'acef', 'afgdb', 'bgdace']}
-              ]
+test_set =  [{'debug_code': ['acedgfb', 'cdfbe', 'gcdfa', 'fbcad', 'dab', 'cefabd', 'cdfgeb', 'eafb', 'cagedb', 'ab'],
+               'output': ['cdfeb', 'fcadb', 'cdfeb', 'cdbaf']}]
 
 seven_digit = {
     'top': None,
@@ -42,7 +29,6 @@ def decrypt_displays(seven_dig_outputs: List[OrderedDict]):
     output = []
     for number_set in seven_dig_outputs:
         output_data = {'input': number_set, 'inverse_legend': {}, 'legend': {}, 'seven_digit': seven_digit, 'decoded_output': []}
-        # sorted_number = ''.join(sorted(number))
         # Find positions in legend.Eight
         for number in [x for x in number_set['debug_code'] if len(x) == 7]:
             output_data['legend']['8'] = ''.join(sorted(number))
@@ -66,11 +52,12 @@ def decrypt_displays(seven_dig_outputs: List[OrderedDict]):
         # Find seven_digit.Middle
         output_data['seven_digit']['middle'] = [x for x in list(output_data['legend']['8']) if x not in list(output_data['legend']['0'])]
         # Find seven_digit.Upper_Left
-        output_data['seven_digit']['upper_left'] = [x for x in output_data['legend']['4'] if x not in output_data['legend']['1'] and x not in output_data['seven_digit']['middle']]
+        output_data['seven_digit']['upper_left'] = [x for x in output_data['legend']['4'] if x not in output_data['legend']['1']
+                                                    and x not in output_data['seven_digit']['middle']]
         # Find positions in legends.Two
         two_potentials = [x for x in number_set['debug_code'] if len(x) == 5]
         for number in two_potentials:
-            if (output_data['seven_digit']['upper_left'] not in list(number)
+            if (output_data['seven_digit']['upper_left'][0] not in list(number)
                 and len([x for x in list(output_data['legend']['1']) if x not in list(number)]) == 1):
                 output_data['legend']['2'] = ''.join(sorted(number))
         # Find seven_digit.Lower_Right
@@ -79,33 +66,27 @@ def decrypt_displays(seven_dig_outputs: List[OrderedDict]):
         output_data['seven_digit']['upper_right'] = [x for x in output_data['legend']['1'] if x not in list(output_data['seven_digit']['lower_right'])]
         # Find positions in legends.Nine
         nine_potentials = [x for x in number_set['debug_code'] if len(x) == 6]
-        print(nine_potentials)
         for number in nine_potentials:
             if len([x for x in list(number) if x not in output_data['legend']['7'] + output_data['legend']['4']]) == 1:
                 output_data['legend']['9'] = ''.join(sorted(number))
-        # # Find seven_digit.Bottom
-        # output_data['seven_digit']['bottom'] = [x for x in output_data['legend']['9'] if x not in output_data['legend']['7'] + output_data['legend']['4']]
-        # # Find seven_digit.Lower_Left
-        # output_data['seven_digit']['lower_left'] = [x for x in output_data['legend']['8'] if x not in output_data['legend']['9']]
-        # # Find positions in legends.Three
-        # output_data['legend']['3'] = ''.join([x for x in output_data['legend']['9'] if x not in output_data['seven_digit']['upper_left']])
-        # # Find positions in legends.Five
-        # output_data['legend']['5'] = ''.join([x for x in output_data['legend']['9'] if x not in output_data['seven_digit']['upper_right']])
-        # # Find positions in legends.Six
-        # output_data['legend']['6'] = ''.join(sorted(list(output_data['legend']['5']) + (output_data['seven_digit']['lower_left'])))
+        # Find seven_digit.Bottom
+        output_data['seven_digit']['bottom'] = [x for x in output_data['legend']['9'] if x not in output_data['legend']['7'] + output_data['legend']['4']]
+        # Find seven_digit.Lower_Left
+        output_data['seven_digit']['lower_left'] = [x for x in output_data['legend']['8'] if x not in output_data['legend']['9']]
+        # Find positions in legends.Three
+        output_data['legend']['3'] = ''.join([x for x in output_data['legend']['9'] if x not in output_data['seven_digit']['upper_left']])
+        # Find positions in legends.Five
+        output_data['legend']['5'] = ''.join([x for x in output_data['legend']['9'] if x not in output_data['seven_digit']['upper_right']])
+        # Find positions in legends.Six
+        output_data['legend']['6'] = ''.join(sorted(list(output_data['legend']['5']) + (output_data['seven_digit']['lower_left'])))
 
 
         # Generate Inverse Legend
         output_data['inverse_legend'] = {value:key for key, value in output_data['legend'].items()}
-        print(output_data['inverse_legend'])
         # Decode output
         for number in number_set['output']:
             sorted_number = ''.join(sorted(number))
-            try:
-                output_data['decoded_output'].append(output_data['inverse_legend'][sorted_number])
-            except:
-                print(f'{sorted_number} appears invalid.')
-                output_data['decoded_output'].append(None)
+            output_data['decoded_output'].append(output_data['inverse_legend'][sorted_number])
         output.append(output_data)
     return output
 
@@ -118,17 +99,20 @@ if __name__ == '__main__':
         frame['output'] =  [x for x in line.split('|')[1].split(' ') if x != '']
         dataset.append(frame)
 
-# pprint(decrypt_displays(test_set))
+    # decrypted_displays = decrypt_displays(test_set) # Test code
+    decrypted_displays = decrypt_displays(dataset)
+    # pprint(decrypted_displays) # Test code
 
-# decrypted_displays = decrypt_displays(dataset)
-decrypted_displays = decrypt_displays(test_set)
+    # Pt1
+    pt1_total = 0
+    for display in decrypted_displays:
+        for item in display['decoded_output']:
+            if item in ['1','4','7','8']:
+                pt1_total += 1
+    print(f'==Part One Answer== \n{pt1_total}')
 
-pprint(decrypted_displays)
-
-# Pt1
-# total = 0
-# for display in decrypted_displays:
-#     for number in display['decoded_output']:
-#         if number in ['1','4','7','8']:
-#             total += 1
-# print(total)
+    # Pt2
+    pt2_total = 0
+    for display in decrypted_displays:
+        pt2_total += int(''.join(display['decoded_output']))
+    print(f'==Part Two Answer== \n{pt2_total}')
