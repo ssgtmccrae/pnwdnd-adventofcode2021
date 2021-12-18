7f28833b2b2ab7edda4f4c9b1c1e9163eb0975ed
Ryan McGregor <ryanmcgr@protonmail.com>
2021-12-18 03:39:50 -0800

    Linting

--- ssgtmccrae/day11/day11.py
+++ ssgtmccrae/day11/day11.py
@@ -5,9 +5,7 @@ https://adventofcode.com/2021/day/11
 """
 
 # from pprint import pprint
-from collections import namedtuple
 from aocd import get_data
-from pprint import pprint
 import numpy as np
 
 TEST_SET_RAW = """5483143223
@@ -21,6 +19,17 @@ TEST_SET_RAW = """5483143223
 4846848554
 5283751526
 """
+
+COMPASS_DIRECTIONS = {
+    'n': (-1, 0),
+    's': (1, 0),
+    'w': (0,-1),
+    'e': (0, 1),
+    'nw': (-1, -1),
+    'sw': (1, -1),
+    'ne': (-1, 1),
+    'se': (1, 1),
+    }
 
 class OctopusGraph():
     """
@@ -36,37 +45,29 @@ class OctopusGraph():
         if isinstance(starting_state, np.ndarray):
             self.state = starting_state
 
-    def tickState(self, number_of_cycles=1):
+    def tick_state(self, number_of_cycles=1):
         """
         Tick state of the Octopus field.
         Input: number_of_cycles (int, default: 1)
         Output: None
         """
-        for x in range(int(number_of_cycles)):
+        iterations = 0
+        while iterations < number_of_cycles:
+            iterations += 1
             self.cycles_ticked += 1
             self.state = np.add(self.state, np.ones(shape=self.state.shape, dtype=int))
             flash_mask = np.ones(self.state.shape)
-            for idx, value in np.ndenumerate(self.state):
+            for idx in np.ndenumerate(self.state):
                 if self.state[idx] * flash_mask[idx] > 9:
-                    self.flashPoint(idx, flash_mask)
+                    self.__flash_point(idx, flash_mask)
             self.state = np.multiply(self.state, flash_mask)
 
-    def getSurroundings(self, point_idx):
+    def __get_surroundings(self, point_idx):
         """
-        Gets points surrounding a point on the OctopusGraph state array.
+        get_s points surrounding a point on the OctopusGraph state array.
         Input: point_idx (tuple(int))
         Output: surroundings (dict)
         """
-        COMPASS_DIRECTIONS = {
-            'n': (-1, 0),
-            's': (1, 0),
-            'w': (0,-1),
-            'e': (0, 1),
-            'nw': (-1, -1),
-            'sw': (1, -1),
-            'ne': (-1, 1),
-            'se': (1, 1),
-            }
         surroundings = {}
         for direction in COMPASS_DIRECTIONS:
             if (point_idx[0] + COMPASS_DIRECTIONS[direction][0] < self.state.shape[0] and
@@ -79,7 +80,7 @@ class OctopusGraph():
                     )
         return surroundings
 
-    def flashPoint(self, point_idx, flash_mask):
+    def __flash_point(self, point_idx, flash_mask):
         """
         Flashes point and surrounding points.
         Input: point (tuple(int))
@@ -87,11 +88,11 @@ class OctopusGraph():
         """
         flash_mask[point_idx] = 0
         self.flashes += 1
-        surroundings = self.getSurroundings(point_idx)
+        surroundings = self.__get_surroundings(point_idx)
         for neighbor_idx in surroundings.values():
             self.state[neighbor_idx] += 1
             if self.state[neighbor_idx] * flash_mask[neighbor_idx] > 9:
-                self.flashPoint(neighbor_idx, flash_mask)
+                self.__flash_point(neighbor_idx, flash_mask)
 
 
 
@@ -108,11 +109,11 @@ if __name__ == '__main__':
 
     # Pt 1
     octopus_field_1 = OctopusGraph(dataset)
-    octopus_field_1.tickState(100)
+    octopus_field_1.tick_state(100)
     print(octopus_field_1.flashes)
 
     # Pt 2
     octopus_field_2 = OctopusGraph(dataset)
     while np.sum(np.sum(octopus_field_2.state)) != 0:
-        octopus_field_2.tickState()
+        octopus_field_2.tick_state()
     print(f'Final Iteration: {octopus_field_2.cycles_ticked}; Sum: {np.sum(np.sum(octopus_field_2.state))}')
